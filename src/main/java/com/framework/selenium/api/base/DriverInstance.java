49753f65827e217904fff7b0a7701820ab2ef13f
Ramya <alramyadevi@gmail.com>
2023-06-18 18:41:37 +0530

    Updated remote web driver

--- src/main/java/com/framework/selenium/api/base/DriverInstance.java
+++ src/main/java/com/framework/selenium/api/base/DriverInstance.java
@@ -1,5 +1,7 @@
 package com.framework.selenium.api.base;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,12 +9,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 public class DriverInstance  extends AbstractTestNGCucumberTests{
+	
 
 	private static final ThreadLocal<RemoteWebDriver> remoteWebdriver = new ThreadLocal<RemoteWebDriver>();
 	private static final ThreadLocal<WebDriverWait> wait = new  ThreadLocal<WebDriverWait>();
@@ -25,13 +29,19 @@ public class DriverInstance  extends AbstractTestNGCucumberTests{
 		return wait.get();
 	}
 
-	public void setDriver(String browser, boolean headless) {		
+	public void setDriver(String browser, boolean headless) throws MalformedURLException {	
+		String URL="http://4.240.86.86:4444/wd/hub";
+		DesiredCapabilities cap=new DesiredCapabilities();
 		switch (browser) {
 		case "chrome":
 			ChromeOptions chrome_options = new ChromeOptions();
 			chrome_options.addArguments("--start-maximized"); 
 			chrome_options.addArguments("--disable-notifications"); 
-			remoteWebdriver.set(new ChromeDriver(chrome_options));
+			//remoteWebdriver.set(new ChromeDriver(chrome_options));
+			cap.setBrowserName("chrome");
+			cap.setVersion("114.0");
+			remoteWebdriver.set(new RemoteWebDriver(new URL(URL), cap));
+					
 			break;
 		case "firefox":
 			FirefoxOptions firefox_options = new FirefoxOptions();
